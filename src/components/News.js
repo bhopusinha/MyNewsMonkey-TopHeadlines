b2faaae41fb98a0c8245c10510c633570b4b5636
bhopusinha <108219196+bhopusinha@users.noreply.github.com>
2023-04-06 12:38:04 +0530

    update my change

--- src/components/News.js
+++ src/components/News.js
@@ -1,94 +1,91 @@
-import React, {useEffect,useState} from 'react'
-import NewsItem from './NewsItem'
+import React, { useEffect, useState } from 'react';
+import NewsItem from './NewsItem';
 import Spiner from './Spiner';
-import PropTypes from 'prop-types'
-import InfiniteScroll from "react-infinite-scroll-component";
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-
-const News=(props)=> {
-   const [articles,setArticals]=useState([])
-   const [loading,setLoading]=useState(true)
-   const [page,setPage]=useState(1)
-   const [totalResults,setTotalResults]=useState(0)
+const News = (props) => {
+  const [articles, setArticals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
   const capitalizerFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
-
-
-
-
-  const update =async ()=>{
-    props.setProgress(0);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=1847c453632c4ce0a3fd0802eda9ef0e&page=${page}&pagesize=${props.pagesize}&category=${props.category}`
-    setLoading(true)
-    let data = await fetch(url)
-    props.setProgress(20);
-    let response = await data.json()
-    props.setProgress(30);
-    setArticals(response.articles)
-    setTotalResults(response.totalResults)
-    setLoading(false)
-    props.setProgress(100);
-  }
-
-  useEffect(()=>{
-   document.title = `${capitalizerFirstLetter(props.category)} - NewsMonkey`
-    update()
-    //eslint-disable-next-line
-  },[]);
-
-  const fetchMoreData = async () => {
-    console.log("Next")
-    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=1847c453632c4ce0a3fd0802eda9ef0e&page=${page+1}&pagesize=${props.pagesize}&category=${props.category}`
-    setPage(page+1)
-    setLoading(true)
-    let data = await fetch(url)
-    let response = await data.json()
-    setArticals(articles.concat(response.articles))
-    setLoading(false)
+    return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-    return (
-      <>
-        <h1 className='text-center' style={{ margin: '40px 0px',marginTop:'90px' }}>NewsMonkey- Top {capitalizerFirstLetter(props.category)} Headlines</h1>
-         {loading && <Spiner/>}
-         {articles && articles.length > 0 && ( <InfiniteScroll
-      dataLength={articles.length}
-      next={fetchMoreData}
-      hasMore={articles.length !== totalResults}
-      loader={<Spiner />}
-    >
-      <div className="container my-4">
-        <div className="row">
-          {articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imageurl={element.urlToImage}
-                  newsurl={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                  source={element.source.name}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </InfiniteScroll>
-)}
+  const update = async () => {
+    props.setProgress(0);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=1847c453632c4ce0a3fd0802eda9ef0e&page=${page}&pagesize=${props.pagesize}&category=${props.category}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(20);
+    let response = await data.json();
+    props.setProgress(30);
+    setArticals(response.articles);
+    setTotalResults(response.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  };
+
+  useEffect(() => {
+    document.title = `${capitalizerFirstLetter(props.category)} - NewsMonkey`;
+    update();
+    //eslint-disable-next-line
+  }, []);
+
+  const fetchMoreData = async () => {
+    console.log('Next');
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=1847c453632c4ce0a3fd0802eda9ef0e&page=${page + 1}&pagesize=${props.pagesize}&category=${props.category}`;
+    setPage(page + 1);
+    setLoading(true);
+    let data = await fetch(url);
+    let response = await data.json();
+    setArticals(articles.concat(response.articles));
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <h1 className='text-center' style={{ margin: '40px 0px', marginTop: '90px' }}>
+        NewsMonkey- Top {capitalizerFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Spiner />}
+      {articles && articles.length > 0 && (
+        <InfiniteScroll dataLength={articles.length} next={fetchMoreData} hasMore={articles.length !== totalResults} loader={<Spiner />}>
+          <div className='container my-4'>
+            <div className='row'>
+              {articles.map((element) => {
+                return (
+                  <div className='col-md-4' key={element.url}>
+                    <NewsItem
+                      title={element.title}
+                      description={element.description}
+                      imageurl={element.urlToImage}
+                      newsurl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      )}
+    </>
+  );
+};
 
 News.defaultProps = {
-  country: "in",
-  pagesize: 8
-}
+  country: 'in',
+  pagesize: 8,
+};
 
 News.propTypes = {
   country: PropTypes.string,
-  pagesize: PropTypes.number
-}
+  pagesize: PropTypes.number,
+};
 
 export default News;
